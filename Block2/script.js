52e3d54f9d10b2de1f5817949e6dd3e04598bc58
thecamboprince <dug21002@byui.edu>
2022-04-07 16:28:44 -0600

    Final

--- Block2/script.js
+++ Block2/script.js
@@ -19,17 +19,32 @@ let weather = {
   displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
+    const { temp, humidity, feels_like } = data.main;
     const { speed } = data.wind;
+
+    //Displays City Name
     document.querySelector(".city").innerText = "Weather in " + name;
+
+    //Displays Icons
     document.querySelector(".icon").src =
       "http://openweathermap.org/img/wn/" + icon + ".png";
+
+    //Displays weather descriptions  
     document.querySelector(".description").innerText = description;
+
+    //Displays current temperature
     document.querySelector(".temp").innerText = Math.ceil((temp * 1.8)+32) + "°F";
-    document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + Math.ceil(speed / 1.609344) + " mph";
+
+    //Displays humidity
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+
+    //Displays wind speed  
+    document.querySelector(".wind").innerText = "Wind speed: " + Math.ceil(speed / 1.609344) + " mph";
+    
+    //Displays feels like temperature
+    document.querySelector(".feelslike").innerText =
+      "Feels Like: " + Math.ceil((feels_like * 1.8)+32) + " °F";
+    
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
